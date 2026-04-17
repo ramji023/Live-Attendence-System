c1a893944ff7699833949b12ee017b355199c433
Ram ji <mishraramji039@gmail.com>
2026-04-17 11:39:14 +0530

    resolve syntax error

--- backend/src/routes/auth/auth.ts
+++ backend/src/routes/auth/auth.ts
@@ -20,7 +20,7 @@ router.post("/login", async (req: Request, res: Response) => {
       name,
       email,
       password,
-      role: email === "admin@example.com" ? "admin" : "employee",,
+      role: email === "admin@example.com" ? "admin" : "employee",
     });
   } else {
     const isMatch = password === employee.password;
